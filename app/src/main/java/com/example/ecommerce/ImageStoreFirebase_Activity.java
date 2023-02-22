9ad44ddd3fac8cb3753704d78fe1b0e9e40bd3e5
Samadhan Patil <109036065+PatilSama@users.noreply.github.com>
2023-02-22 15:15:56 +0530

    firebase

--- app/src/main/java/com/example/ecommerce/ImageStoreFirebase_Activity.java
+++ app/src/main/java/com/example/ecommerce/ImageStoreFirebase_Activity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.app.ProgressDialog;
@@ -13,14 +14,19 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -30,24 +36,57 @@ import java.util.UUID;
 
 public class ImageStoreFirebase_Activity extends AppCompatActivity {
 
-    Button btnUpload,btnCamera;
+    Button btnUpload,btnCamera,btnRetrieve;
     ImageView imgView;
     FirebaseDatabase fd;
     Bitmap bitmap;
     Uri uri;
+//    private List<Upload> uploads;
     FirebaseStorage firebaseStorage;
+    private DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_store_firebase);
         btnUpload = findViewById(R.id.btnUpload);
         btnCamera = findViewById(R.id.btnCamera);
+        btnRetrieve = findViewById(R.id.btnRetrieve);
         imgView = findViewById(R.id.imgView);
 
         fd = FirebaseDatabase.getInstance();
         firebaseStorage = FirebaseStorage.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
 
+//        btnRetrieve.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                databaseReference.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(DataSnapshot snapshot) {
+//                        //dismissing the progress dialog
+//                        progressDialog.dismiss();
+//
+//                        //iterating through all the values in database
+//                        for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+//                            Upload upload = postSnapshot.getValue(Upload.class);
+//                            uploads.add(upload);
+//                        }
+//                        //creating adapter
+//                        adapter = new MyAdapter(getApplicationContext(), uploads);
+//
+//                        //adding adapter to recyclerview
+//                        recyclerView.setAdapter(adapter);
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(DatabaseError databaseError) {
+//                        progressDialog.dismiss();
+//                    }
+//                });
+//
+//            }
+//        });
         btnUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,4 +153,31 @@ public class ImageStoreFirebase_Activity extends AppCompatActivity {
         }
     }
 
+}
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder>
+{
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.recycleviewdata,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+
+        }
+    }
 }
